997d78b54c202530a4db7a4369de9f5d0d5cd65d
harshala73 <126048878+harshala73@users.noreply.github.com>
2023-02-22 12:15:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.text('🍑Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
 
-treamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
