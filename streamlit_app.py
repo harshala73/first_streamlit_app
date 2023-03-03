43c0baa7a8b47a7486af6e4697b35e9073003e04
harshala73 <126048878+harshala73@users.noreply.github.com>
2023-03-03 12:12:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,75 @@
 import streamlit
-
-streamlit.title('My parent new healtrhy diner')
-
-streamlit.header('Breakfast Favourites')
-streamlit.text('🍑Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥚Hard-Boiled Free-Range Egg')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+import requests
+import snowflake.connector
+from urllib.error import URLError
+streamlit.title('My Mom New Healthy Diner')
 
+streamlit.header( 'Breakfast fav')
+streamlit.text( ' omega 3 and blueberry oatmeal')
+streamlit.text('kale, spinach and smoothie')
+streamlit.text('hard-boiled free-range egg')
+
+streamlit.header( 'BUILD YOUR OWN SMOOTHIE')
+
+#import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado'])
-                                                                      
-fruits_to_show = my_fruit_list.loc[fruits_selected]                                                                     
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+
+#Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+      
+   
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+    streamlit.error("please slect a fruit to get information.")
+   else:
+    back_from_function=get_fruityvice_data(this_fruit_choice)
+    streamlit.dataframe(back_from_function)
+#streamlit.write('The user entered ', fruit_choice)
+except URLError as e:
+  streamlit.error()
 
 
-streamlit.dataframe(my_fruit_list)
 
-import snowflake.connector
+streamlit.header("VIEW OUR FRUIT LIST-ADD YOUR FAVOURITES!")
+#snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("SELECT * from fruit_load_list")
+         return my_cur.fetchall()
+    
+    #add a button to load a fruit
+if streamlit.button('Get fruit list'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     my_data_rows = get_fruit_load_list()
+     my_cnx.close()
+     streamlit.dataframe(my_data_rows)
+#allow the end user to add fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values(' "+add_my_fruit+" ')")
+         return "Thanks for adding"+ new_fruit
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+add_my_fruit = streamlit.text_input('What  would you like to add?')
+if streamlit.button('add a fruit to the list'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     back_from_function=insert_row_snowflake(add_my_fruit)
+     streamlit.text(back_from_function)                                
+     streamlit.write('The user entered ', add_my_fruit)
 
-streamlit.header('Fruityvise Fruit Advise')
+#streamlit.write('Thanks for adding','jackfruit')
+#streamlit.write('The user entered ', jackfruit)
+streamlit.stop() 
